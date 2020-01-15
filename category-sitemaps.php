33adc65700a8c741507afa7a292198d211089c42
3ele-projects <github@3ele.de>
2020-01-15 12:58:42 +0100

    add master sitemap

--- category-sitemaps.php
+++ category-sitemaps.php
@@ -17,7 +17,16 @@
 function create_sitemap($post_ID) {
     $categories =  get_the_category($post_ID );
     $sitemaps = array();
+    $update_time = date("Y-m-d");
+    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+
     foreach($categories as $category) {
+        $sitemap .= "\t" . '<url>' . "\n" .
+        "\t\t" . '<loc>/' . get_category_link($category) . '</loc>' .
+       "\n\t\t" . '<lastmod>' . $update_time . '</lastmod>' .
+       "\n\t" . '</url>' . "\n";
+
+
         $posts_for_sitemap = get_posts( array(
             'numberposts' => -1,
             'orderby' => 'modified',
@@ -25,43 +34,45 @@ function create_sitemap($post_ID) {
             'category' => $category->ID,
             'post_type' => array( 'post' )
         ));
-    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
     foreach( $posts_for_sitemap as $post ) {
         setup_postdata( $post );
         $postdate = explode( " ", $post->post_modified );
         $sitemap .= "\t" . '<url>' . "\n" .
              "\t\t" . '<loc>' . get_permalink( $post->ID ) . '</loc>' .
             "\n\t\t" . '<lastmod>' . $postdate[0] . '</lastmod>' .
-            "\n\t\t" . '<changefreq>monthly</changefreq>' .
             "\n\t" . '</url>' . "\n";
     }
     $sitemap .= '</urlset>';
     $fp = fopen( ABSPATH . $category->name."-sitemap.xml", 'w' );
     fwrite( $fp, $sitemap );
     fclose( $fp );
-    $url = ABSPATH . $category->name."-sitemap.xml"
+    $url = ABSPATH . $category->name."-sitemap.xml";
     $sitemaps[] = $url;
-
-}
  if(!empty($sitemaps)) {
     create_master_sitemap();
  }
 }
+}
+
 
 function create_master_sitemap($sitemaps){
-    $files = glob(ABSPATH .'*.-sitemap.xml');
+    $files = glob('*sitemap.xml');
+    $update_time = date("Y-m-d");
+
     $master_sitemap =' <?xml version="1.0" encoding="UTF-8"?>' . "\n" .' <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'."\n";
     foreach($files as $sitemap) { 
         $master_sitemap .='
     <sitemap>
        <loc>'.$sitemap.'</loc>
-    </sitemap>'
+       <lastmod>' . $update_time  . '</lastmod>
+
+    </sitemap>';
     };   
-    $master_sitemap.= '</sitemapindex>'  
+    $master_sitemap.= '</sitemapindex>';
+    $fp = fopen( ABSPATH ."catsitemap.xml", 'w' );
+    fwrite( $fp, $master_sitemap);
+    fclose( $fp ); 
 };
 
 add_action( 'publish_post', 'create_sitemap' ); 
 add_action( 'save_post', 'create_sitemap' ); 
-
-
-
